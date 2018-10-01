e9900b214656377be7c66e0375f7b8f87771c773
claudiocarvalhojr <claudio.omar.jr@gmail.com>
2018-10-01 00:07:03 -0300

    Added an installer

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.Color;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -18,12 +19,10 @@ import main.resources.Game;
 import main.resources.Player;
 import main.resources.Rain;
 
-
 /**
  *
  */
-public class Main
-{
+public class Main {
 
 	private static JFrame frame;
 	private static JPanel panel;
@@ -32,14 +31,11 @@ public class Main
 	/**
 	 * @param args
 	 */
-	public static void main(final String[] args)
-	{
-		try
-		{
+	public static void main(final String[] args) {
+		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		}
-		catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e)
-		{
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
 			e.printStackTrace();
 		}
 		initialize();
@@ -47,17 +43,34 @@ public class Main
 
 	private static void initialize()
 	{
-		frame = new JFrame();
-		frame.setSize(405, 302);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(getJPanel());
-		frame.setVisible(true);
-		start();
-		motion(game.getPlayer().getWidth(), game.getPlayer().getHeight());
+		String[] options = {"instalar", "jogar", "cancelar"};
+		String question = "Você deseja " + options[0] + " ou " + options[1] + " pac-fruit?";
+		final int input = JOptionPane.showOptionDialog(null, question, "Pac-fruit", JOptionPane.DEFAULT_OPTION,
+				JOptionPane.QUESTION_MESSAGE, new ImageIcon(Main.class.getResource("/main/resources/img/pac-fruit.png")), options, options[0]);
+		if (input == 0)
+		{
+		
+			try {
+				Instalador.initialize(false, "main/resources/properties/config.properties");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		} else if (input == 1) {
+		
+			frame = new JFrame();
+			frame.setSize(405, 302);
+			frame.setResizable(false);
+			frame.setLocationRelativeTo(null);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.getContentPane().add(getJPanel());
+			frame.setVisible(true);
+			start();
+			motion(game.getPlayer().getWidth(), game.getPlayer().getHeight());
+		
+		}
 	}
-	
+
 	private static JPanel getJPanel() {
 		if (panel == null) {
 			panel = new JPanel();
@@ -69,10 +82,8 @@ public class Main
 		return panel;
 	}
 
-	private static void start()
-	{
-		if (game == null)
-		{
+	private static void start() {
+		if (game == null) {
 			game = new Game();
 			game.setLevel(1);
 			game.setHigherSpeed(5);
@@ -92,83 +103,66 @@ public class Main
 		refreshInfo();
 	}
 
-	private static void restart()
-	{
+	private static void restart() {
 		game = null;
 		start();
 	}
 
-	private static void nextLevel()
-	{
-		if (game.getPlayer() != null)
-		{
+	private static void nextLevel() {
+		if (game.getPlayer() != null) {
 			game.getPlayer().setLifes(game.getPlayer().getLifes() + 1);
 			game.setLevel(game.getLevel() + 1);
 			game.setQtyRain(game.getQtyRain() + 1);
 			start();
-		}
-		else
-		{
+		} else {
 			restart();
 		}
 	}
 
-	private static void stop()
-	{
+	private static void stop() {
 		game.setListFruits(null);
 		game.setActive(false);
 		refreshPanel();
 		getJPanel().removeAll();
 	}
 
-	private static void refreshPanel()
-	{
+	private static void refreshPanel() {
 		getJPanel().revalidate();
 		getJPanel().repaint();
 	}
 
-	private static void refreshInfo()
-	{
+	private static void refreshInfo() {
 		frame.setTitle("Nível: " + game.getLevel() + " | Vidas: " + game.getPlayer().getLifes() + " | Frutas: "
 				+ game.getCountTotalFruit() + " | Atual: " + game.getCountFruit());
 	}
 
-	private static void motion(final int moveRL, final int moveUD)
-	{
-		frame.addKeyListener(new KeyAdapter()
-		{
+	private static void motion(final int moveRL, final int moveUD) {
+		frame.addKeyListener(new KeyAdapter() {
 			@Override
-			public void keyReleased(final KeyEvent event)
-			{
-				if (game.getPlayer() != null && game.getListFruits() != null && !game.getListFruits().isEmpty())
-				{
-					if (event.getKeyCode() == 37 && (game.getPlayer().getLocation().x - moveRL) > 0)
-					{
-						game.getPlayer().setLocation(game.getPlayer().getLocation().x - moveRL, game.getPlayer().getLocation().y);
+			public void keyReleased(final KeyEvent event) {
+				if (game.getPlayer() != null && game.getListFruits() != null && !game.getListFruits().isEmpty()) {
+					if (event.getKeyCode() == 37 && (game.getPlayer().getLocation().x - moveRL) > 0) {
+						game.getPlayer().setLocation(game.getPlayer().getLocation().x - moveRL,
+								game.getPlayer().getLocation().y);
 						collisionFruit(game.getListFruits().size());
-					}
-					else if (event.getKeyCode() == 38 && (game.getPlayer().getLocation().y - moveUD) > 0)
-					{
-						game.getPlayer().setLocation(game.getPlayer().getLocation().x, game.getPlayer().getLocation().y - moveUD);
+					} else if (event.getKeyCode() == 38 && (game.getPlayer().getLocation().y - moveUD) > 0) {
+						game.getPlayer().setLocation(game.getPlayer().getLocation().x,
+								game.getPlayer().getLocation().y - moveUD);
 						collisionFruit(game.getListFruits().size());
-					}
-					else if (event.getKeyCode() == 39 && (game.getPlayer().getLocation().x + moveRL) <= 385)
-					{
-						game.getPlayer().setLocation(game.getPlayer().getLocation().x + moveRL, game.getPlayer().getLocation().y);
+					} else if (event.getKeyCode() == 39 && (game.getPlayer().getLocation().x + moveRL) <= 385) {
+						game.getPlayer().setLocation(game.getPlayer().getLocation().x + moveRL,
+								game.getPlayer().getLocation().y);
 						collisionFruit(game.getListFruits().size());
-					}
-					else if (event.getKeyCode() == 40 && (game.getPlayer().getLocation().y + moveUD) <= 256)
-					{
-						game.getPlayer().setLocation(game.getPlayer().getLocation().x, game.getPlayer().getLocation().y + moveUD);
+					} else if (event.getKeyCode() == 40 && (game.getPlayer().getLocation().y + moveUD) <= 256) {
+						game.getPlayer().setLocation(game.getPlayer().getLocation().x,
+								game.getPlayer().getLocation().y + moveUD);
 						collisionFruit(game.getListFruits().size());
 					}
 				}
-				if (event.getKeyCode() == 27)
-				{
-					final int input = JOptionPane.showConfirmDialog(panel, "Deseja sair?", "Pac-fruit", JOptionPane.YES_NO_OPTION,
-							JOptionPane.QUESTION_MESSAGE);
-					if (input == 0)
-					{
+				if (event.getKeyCode() == 27) {
+					final int input = JOptionPane.showConfirmDialog(panel, "Deseja sair?", "Pac-fruit",
+							JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+					if (input == 0) {
 						System.exit(0);
 					}
 				}
@@ -176,35 +170,27 @@ public class Main
 		});
 	}
 
-	private static void collisionFruit(final int listFruitSize)
-	{
-		if (game.getListFruits() != null && game.getPlayer() != null && !game.getListFruits().isEmpty())
-		{
-			for (int count = 0; count < listFruitSize; count++)
-			{
+	private static void collisionFruit(final int listFruitSize) {
+		if (game.getListFruits() != null && game.getPlayer() != null && !game.getListFruits().isEmpty()) {
+			for (int count = 0; count < listFruitSize; count++) {
 				if (game.getPlayer().getLocation().x == game.getListFruits().get(count).getLocation().x
 						&& game.getPlayer().getLocation().y == game.getListFruits().get(count).getLocation().y
-						&& game.getCountFruit() < 3)
-				{
+						&& game.getCountFruit() < 3) {
 					refreshPanel();
 					getJPanel().remove(game.getListFruits().get(count));
 					game.setCountFruit(game.getCountFruit() + 1);
 					game.setCountTotalFruit(game.getCountTotalFruit() + 1);
 					refreshInfo();
-					if (game.getCountFruit() == game.getQtyFruit())
-					{
+					if (game.getCountFruit() == game.getQtyFruit()) {
 						stop();
 						final int input = JOptionPane.showConfirmDialog(panel,
-								"Você ganhou!\n\nColetou: " + game.getCountTotalFruit() + " frutas\nNível: " + game.getLevel()
-										+ "\n\n Jogar o próximo nível?",
+								"Você ganhou!\n\nColetou: " + game.getCountTotalFruit() + " frutas\nNível: "
+										+ game.getLevel() + "\n\n Jogar o próximo nível?",
 								"Pac-fruit", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-						if (input == 0)
-						{
+						if (input == 0) {
 
 							nextLevel();
-						}
-						else
-						{
+						} else {
 							System.exit(0);
 						}
 					}
@@ -213,27 +199,22 @@ public class Main
 		}
 	}
 
-	private static boolean collisionRain(final Rain rain)
-	{
+	private static boolean collisionRain(final Rain rain) {
 		if (rain != null && game.getPlayer() != null && rain.getLocation().x == game.getPlayer().getLocation().x
-				&& rain.getLocation().y == game.getPlayer().getLocation().y && game.getPlayer().getLifes() > 0)
-		{
+				&& rain.getLocation().y == game.getPlayer().getLocation().y && game.getPlayer().getLifes() > 0) {
 			game.getPlayer().setLifes(game.getPlayer().getLifes() - 1);
 			refreshPanel();
 			getJPanel().remove(rain);
 			refreshInfo();
-			if (game.getPlayer().getLifes() == 0)
-			{
+			if (game.getPlayer().getLifes() == 0) {
 				stop();
-				final int input = JOptionPane.showConfirmDialog(panel, "Você perdeu!\n\nColetou: " + game.getCountTotalFruit()
-						+ " frutas\nNível: " + game.getLevel() + "\n\nDeseja jogar novamente?", "Pac-fruit", JOptionPane.YES_NO_OPTION,
-						JOptionPane.ERROR_MESSAGE);
-				if (input == 0)
-				{
+				final int input = JOptionPane.showConfirmDialog(panel,
+						"Você perdeu!\n\nColetou: " + game.getCountTotalFruit() + " frutas\nNível: " + game.getLevel()
+								+ "\n\nDeseja jogar novamente?",
+						"Pac-fruit", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+				if (input == 0) {
 					restart();
-				}
-				else
-				{
+				} else {
 					System.exit(0);
 				}
 			}
@@ -242,10 +223,8 @@ public class Main
 		return false;
 	}
 
-	private static void makePlayer()
-	{
-		if (game.getPlayer() == null)
-		{
+	private static void makePlayer() {
+		if (game.getPlayer() == null) {
 			game.setPlayer(new Player());
 			game.getPlayer().setLifes(5);
 			game.getPlayer().setWidth(12);
@@ -258,29 +237,22 @@ public class Main
 		getJPanel().add(game.getPlayer());
 	}
 
-	private static void makeFruit(final int qtyFruit)
-	{
+	private static void makeFruit(final int qtyFruit) {
 		final Random random = new Random();
 		int column = 1;
 		int row = 1;
-		for (int count = 0; count < qtyFruit; count++)
-		{
+		for (int count = 0; count < qtyFruit; count++) {
 			final Fruit fruit = new Fruit();
 			fruit.setWidth(12);
 			fruit.setHeight(15);
 			fruit.setIcon(new ImageIcon(Main.class.getResource("/main/resources/img/fruit.png")));
-			do
-			{
+			do {
 				column = random.nextInt(385) + 1;
-			}
-			while (column > 0 && ((column - 1) % fruit.getWidth() != 0));
-			do
-			{
+			} while (column > 0 && ((column - 1) % fruit.getWidth() != 0));
+			do {
 				row = random.nextInt(256) + 1;
-			}
-			while (row > 0 && ((row - 1) % fruit.getHeight() != 0));
-			if (column == 1 && row == 256)
-			{
+			} while (row > 0 && ((row - 1) % fruit.getHeight() != 0));
+			if (column == 1 && row == 256) {
 				column += 12;
 			}
 			fruit.setBounds(column, row, fruit.getWidth(), fruit.getHeight());
@@ -290,53 +262,38 @@ public class Main
 		}
 	}
 
-	private static void makeRain(final int qtyRain)
-	{
-		for (int i = 0; i < qtyRain; i++)
-		{
-			new Thread()
-			{
+	private static void makeRain(final int qtyRain) {
+		for (int i = 0; i < qtyRain; i++) {
+			new Thread() {
 				@Override
-				public void run()
-				{
+				public void run() {
 					Rain rain = null;
 					final Random random = new Random();
 					int speed = 0;
 					int column = 0;
-					while (game.isActive())
-					{
-						try
-						{
-							if (rain == null)
-							{
+					while (game.isActive()) {
+						try {
+							if (rain == null) {
 								rain = new Rain();
 								rain.setWidth(12);
 								rain.setHeight(15);
 							}
 							getJPanel().add(rain);
-							do
-							{
+							do {
 								column = random.nextInt(385) + 1;
-							}
-							while (column > 1 && ((column - 1) % rain.getWidth() != 0));
+							} while (column > 1 && ((column - 1) % rain.getWidth() != 0));
 							speed = random.nextInt(2);
-							for (int row = 1; row <= 257; row++)
-							{
-								if (game.isActive())
-								{
+							for (int row = 1; row <= 257; row++) {
+								if (game.isActive()) {
 									rain.setIcon(new ImageIcon(getClass().getResource("/main/resources/img/rain.png")));
 									rain.setBounds(column, row, rain.getWidth(), rain.getHeight());
 									rain.setVisible(true);
-									if (speed == 0)
-									{
+									if (speed == 0) {
 										Thread.sleep(game.getHigherSpeed());
-									}
-									else if (speed == 1)
-									{
+									} else if (speed == 1) {
 										Thread.sleep(game.getLowerSpeed());
 									}
-									if (collisionRain(rain))
-									{
+									if (collisionRain(rain)) {
 										rain = null;
 										return;
 									}
@@ -345,9 +302,7 @@ public class Main
 							refreshPanel();
 							getJPanel().remove(rain);
 							rain = null;
-						}
-						catch (final Exception e)
-						{
+						} catch (final Exception e) {
 							e.printStackTrace();
 						}
 					}
